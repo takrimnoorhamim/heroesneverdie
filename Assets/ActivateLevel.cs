08b69c12a63b68fbfd3917d28b28011e53562ea2
takrimnoorhamim <takrimnoorhamimpass@gmail.com>
2024-10-17 14:58:34 +0600

    hey

--- Assets/ActivateLevel.cs
+++ Assets/ActivateLevel.cs
@@ -3,21 +3,37 @@ using UnityEngine.UI;
 
 public class ActivateGameObject : MonoBehaviour
 {
-    public GameObject targetObject; // The GameObject to activate
+    public GameObject toActivate; // The GameObject to activate
     private Button button;
-    public GameObject Logos;
+    public GameObject toDeactivate;
 
     void Start()
     {
         // Get the Button component and add a listener to the button click event
         button = GetComponent<Button>();
-        button.onClick.AddListener(OnButtonClick);
+        if (button != null)
+        {
+            button.onClick.AddListener(OnButtonClick);
+        }
     }
 
     void OnButtonClick()
     {
-        // Activate the target GameObject
-        targetObject.SetActive(true);
-        Logos.SetActive(false);
+        // Activate the target GameObject if it exists
+        if (toActivate != null)
+        {
+            toActivate.SetActive(true);
+        }
+
+        // Deactivate Logos if it exists
+        if (toDeactivate != null)
+        {
+            toDeactivate.SetActive(false);
+        }
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
